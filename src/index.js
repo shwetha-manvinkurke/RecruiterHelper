ece9c615e9db15f86f8b0c53b514c164e0b825bc
Shwetha Manuvinakurike <shwetha.manvinkurke@gmail.com>
2020-10-18 22:52:13 -0700

    Files

--- src/index.js
+++ src/index.js
@@ -4,6 +4,52 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+class CandidiateQuestionaire extends React.Component {
+  // The questions need to come from the database
+  render() {
+    return (
+      <form>
+        <h1> Questions </h1>
+      </form>
+    )
+  }
+}
+
+class JobForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <h1> Job details </h1>
+      </form>
+    )
+  }
+}
+
+class AllJobs extends React.Component{
+  render() {
+    return (
+      <h1> Return all jobs with candidates link here </h1>
+    )
+  }
+}
+
+class AllCandidates extends React.Component {
+  render() {
+    return (
+      <h1> All candidates with job links here </h1>
+    )
+  }
+}
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <h1> Home page items go here </h1>
+    )
+  }
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
